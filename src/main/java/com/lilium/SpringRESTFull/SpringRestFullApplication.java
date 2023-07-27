d0d386eaa839157de4cdeb09ca07b4df088fc9dd
mhasanovic <2170007646@iau.edu.sa>
2023-07-27 17:12:13 +0200

    Commit-1 Created spring initializr project

---
+++ src/main/java/com/lilium/SpringRESTFull/SpringRestFullApplication.java
@@ -0,0 +1,13 @@
+package com.lilium.SpringRESTFull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestFullApplication.class, args);
+	}
+
+}
